231e147fae5b53c11ec52121e6195eb16179ee20
sweetyray29 <sweetyray29@gmail.com>
2026-09-04 12:18:29 +0530

    Add Medium Java Solution for 81. Search in Rotated Sorted Array II from LeetCode

--- LeetCode/Java/Medium/81. Search in Rotated Sorted Array II/README.md
+++ LeetCode/Java/Medium/81. Search in Rotated Sorted Array II/README.md
@@ -8,8 +8,8 @@
 Array, Binary Search
 
 ### 🚀 Performance
-- **Runtime:** 0 ms
-- **Memory:** 44.6 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/Patterns/Binary Search/81. Search in Rotated Sorted Array II/Solution.java
+++ LeetCode/Patterns/Binary Search/81. Search in Rotated Sorted Array II/Solution.java
@@ -1,9 +1,17 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        int len=nums.length;
-        for(int i=0;i<len;i++){
-            if(nums[i]==target){
+        int start = 0;
+        int end=nums.length-1;
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
                 return true;
+            }
+            if(nums[mid]<target){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
             }
         }
         return false;
